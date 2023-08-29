8a2d1bf920de9d1021c70f534c13b2b963fc9875
ghou8s <ghou8s@gmail.com>
2023-08-29 04:15:45 +0000

    ci: qassa sagit

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/beryllium/qassa-*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/beryllium/ && ls qassa-*.zip)"
-      rclone copy ~/rom/out/target/product/beryllium/qassa-*.zip ghou8s-ci:beryllium -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/beryllium/ && ls qassa-*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/sagit/qassa-*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/sagit/ && ls qassa-*.zip)"
+      rclone copy ~/rom/out/target/product/sagit/qassa-*.zip ghou8s-ci:sagit -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/sagit/ && ls qassa-*.zip) Uploaded Successfully!"
 fi
