2fe289803619de1df940a1d7d61ec749d2390600
deny2311 <deny2311@gmail.com>
2023-09-27 16:57:42 +0700

    cirrus: update .cirrus.yml

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/sagit/qassa*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/sagit/ && ls qassa*.zip)"
-      rclone copy ~/rom/out/target/product/sagit/qassa*.zip deny2311:sagit -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/sagit/ && ls qassa*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/sagit/evolution*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/sagit/ && ls evolution*.zip)"
+      rclone copy ~/rom/out/target/product/sagit/evolution*.zip sagit:evolutionx -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/sagit/ && ls evolution*.zip) Uploaded Successfully!"
 fi
