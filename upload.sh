1c18fa6cd560191eeeb9072e68ceecbbf281d4c2
deny2311 <deny2311@gmail.com>
2023-09-28 10:43:22 +0700

    cirrus: derpfest build

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/sagit/evolution*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/sagit/ && ls evolution*.zip)"
-      rclone copy ~/rom/out/target/product/sagit/evolution*.zip sagit:evolutionx -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/sagit/ && ls evolution*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/sagit/DerpFest*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/sagit/ && ls DerpFest*.zip)"
+      rclone copy ~/rom/out/target/product/sagit/DerpFest*.zip sagit:derpfest -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/sagit/ && ls DerpFest*.zip) Uploaded Successfully!"
 fi
