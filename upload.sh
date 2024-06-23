4a196f317db7158607c67a97d180623cfcfdfb52
thrdchld <109644096+thrdchld@users.noreply.github.com>
2024-06-23 10:34:03 +0700

    Update upload.sh [skip ci]

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/sagit/qassa*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/sagit/ && ls qassa*.zip)"
-      rclone copy ~/rom/out/target/product/sagit/qassa*.zip sagit:qassa -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/sagit/ && ls qassa*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/X01BD/qassa*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/X01BD/ && ls qassa*.zip)"
+      rclone copy ~/rom/out/target/product/X01BD/qassa*.zip qassa:qassa -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/X01BD/ && ls qassa*.zip) Uploaded Successfully!"
 fi
