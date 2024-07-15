58128080711c1ed75386948777a99d46f49cff39
thrdchld <109644096+thrdchld@users.noreply.github.com>
2024-07-15 22:36:20 +0700

    Update upload.sh [skip ci]

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/riva/qassa*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/riva/ && ls qassa*.zip)"
-      curl bashupload.com -T ~/rom/out/target/product/riva/qassa*.zip
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/riva/ && ls qassa*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/X01BD/crDroid*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/X01BD/ && ls crDroid*.zip)"
+      curl bashupload.com -T ~/rom/out/target/product/X01BD/crDroid*.zip
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/X01BD/ && ls crDroid*.zip) Uploaded Successfully!"
 fi
